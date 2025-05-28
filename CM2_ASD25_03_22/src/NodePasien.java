1f93979105786729976436b37a3860d976556fc7
qulbiazz <qulbyazzumy@gmail.com>
2025-05-28 09:27:21 +0700

    node

---
+++ CM2_ASD25_03_22/src/NodePasien.java
@@ -0,0 +1,18 @@
+public class NodePasien {
+    NodePasien next,prev;
+    Dokter dataDokter;
+    Pasien dataPasien;
+    TransaksiLayanan dataTransaksi;
+    Node(Node prev, Pasien dataPasien,Node next){
+        this.next=next;
+        this.prev=prev;
+        this.dataPasien=dataPasien;
+    }
+    Node(Node prev, Dokter dataDokter,Node next){
+        this.next=next;
+        this.prev=prev;
+        this.dataDokter=dataDokter;
+    }
+
+
+}
