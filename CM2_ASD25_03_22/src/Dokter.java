ea2cb202ae37216aefe686d3056664e58b1f009e
qulbiazz <qulbyazzumy@gmail.com>
2025-05-28 08:48:59 +0700

    Dokter_Pasien commit

--- CM2_ASD25_03_22/src/Dokter.java
+++ CM2_ASD25_03_22/src/Dokter.java
@@ -1,3 +1,9 @@
 public class Dokter {
-    
+    String idDokter;
+    String nama;
+
+    Dokter(String id, String nama) {
+        id = idDokter;
+        this.nama = nama;
+    }
 }
--- CM2_ASD25_03_22/src/Pasien.java
+++ CM2_ASD25_03_22/src/Pasien.java
@@ -1,4 +1,16 @@
 public class Pasien {
+    String nama;
+    String nik;
+    String keluhan;
 
-    
+    Pasien(String nama, String nik, String keluhan) {
+        this.nama = nama;
+        this.nik = nik;
+        this.keluhan = keluhan;
+    }
+
+    public void tampilInformasi() {
+        System.out.printf("%-10s %-8s %-10s", "Nama, NIK, Keluhan");
+    }
+
 }
