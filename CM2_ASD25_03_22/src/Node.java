4ab3d1d87f67cca17900e488cb896345e8e8b671
FALAA-1285 <ahmadfalahi1285@gmail.com>
2025-05-28 09:42:11 +0700

    Perbaikan

--- CM2_ASD25_03_22/src/Node.java
+++ CM2_ASD25_03_22/src/Node.java
@@ -3,11 +3,22 @@ public class Node {
     Dokter dataDokter;
     Pasien dataPasien;
     TransaksiLayanan dataTransaksi;
-    Node(Node prev, Pasien dataPasien,Dokter dataDokter, TransaksiLayanan dataTransaksiLayanan, Node next){
+    Node(Node prev, Pasien dataPasien, Node next){
         this.next=next;
         this.prev=prev;
         this.dataPasien=dataPasien;
     }
+    Node(Node prev, Dokter dataDokter, Node next){
+        this.next=next;
+        this.prev=prev;
+        this.dataDokter=dataDokter;
+    }
+    Node(Node prev, TransaksiLayanan dataTransaksi, Node next){
+        this.next=next;
+        this.prev=prev;
+        this.dataTransaksi=dataTransaksi;
+    }
+
 
 
 
