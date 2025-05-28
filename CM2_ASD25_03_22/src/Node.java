4886c5aecc8c3acbec99847f9f8c1996315b6a08
FALAA-1285 <ahmadfalahi1285@gmail.com>
2025-05-28 08:43:06 +0700

    Node & TransasiLayanan

--- CM2_ASD25_03_22/src/Node.java
+++ CM2_ASD25_03_22/src/Node.java
@@ -1,3 +1,14 @@
 public class Node {
-    
+    Node next,prev;
+    Dokter dataDokter;
+    Pasien datapPasien;
+    TransaksiLayanan dataTransaksi;
+    Node(){
+        this.next=next;
+        this.prev=prev;
+        this.dataDokter=dataDokter;
+        this.dataTransaksi=dataTransaksi;
+    }
+
+
 }
--- CM2_ASD25_03_22/src/TransaksiLayanan.java
+++ CM2_ASD25_03_22/src/TransaksiLayanan.java
@@ -1,3 +1,15 @@
 public class TransaksiLayanan {
-    
+    Pasien pasien;
+    Dokter dokter;
+    int durasilayanan;
+    int biaya;
+    TransaksiLayanan(Pasien pasien, Dokter dokter, int durasilayanan, int biaya){
+        this.pasien=pasien;
+        this.dokter=dokter;
+        this.durasilayanan=durasilayanan;
+        this.biaya=biaya;
+    }
+    public int biaya(int durasilayanan){
+        return 50_000*durasilayanan;
+    }
 }
